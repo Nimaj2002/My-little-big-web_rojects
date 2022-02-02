145e7006b9d92caad16328f36524c530f7c1193b
Nima Jelodari <nimajelodari2002@gmail.com>
2022-02-02 22:40:02 +0330

    error fixed

--- Scroll_animation/script.js
+++ Scroll_animation/script.js
@@ -1,5 +1,14 @@
 const boxes = document.querySelectorAll('.box')
 
+let triggerBottom = window.innerHeight / 5 * 4
+boxes.forEach(box => {
+    let boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom){
+        box.classList.add("show")
+    }
+})
+
 window.addEventListener("scroll", checkBoxes)
 
 function checkBoxes(){
