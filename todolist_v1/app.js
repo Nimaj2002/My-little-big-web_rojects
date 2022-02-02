5574fefbdda7a9d0d8d26b18951f3a567570ad3f
Nima Jelodari <nimajelodari2002@gmail.com>
2022-02-02 22:32:41 +0330

    little programss

---
+++ todolist_v1/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    let currentDay = today.getDate();
+    let day = "";
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend"
+    } else {
+        day = "Weekday"
+    }
+    res.render("list", {kindOfDay: day})
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000")
+})
